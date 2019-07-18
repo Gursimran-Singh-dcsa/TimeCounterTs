53cc839d5638f527dce8e7dc05828d491cc89bfa
gursimran singh <gursimransingh8075@gmail.com>
2019-07-18 17:25:01 +0530

    make current time functional

--- TimeCounter.js
+++ TimeCounter.js
@@ -1,11 +1,37 @@
 console.log("initialize");
 var timeDiffgetter = /** @class */ (function () {
     function timeDiffgetter() {
+        this.startTime = '';
+        this.endTime = '';
     }
+    timeDiffgetter.prototype.currentTimeFillMethod = function () {
+        var today = new Date();
+        this.startTime = ((today.getHours() < 10) ? "0" : "") + today.getHours() + ":" + ((today.getMinutes() < 10) ? "0" : "") + today.getMinutes() + ':' + ((today.getSeconds() < 10) ? "0" : "") + today.getSeconds();
+        this.setInput("startTime", this.startTime);
+    };
+    timeDiffgetter.prototype.setInnerHTML = function (typeValue, value, type) {
+        if (type === void 0) { type = "id"; }
+        console.log("i am called", type, typeValue, value);
+        switch (type) {
+            case 'id':
+                document.getElementById(typeValue).innerHTML = value;
+                break;
+        }
+    };
+    timeDiffgetter.prototype.setInput = function (typeValue, value, type) {
+        if (type === void 0) { type = "id"; }
+        console.log("i am called", type, typeValue, value);
+        switch (type) {
+            case 'id':
+                document.getElementById(typeValue).value = value;
+                break;
+        }
+    };
     return timeDiffgetter;
 }());
-var currentTimeDiff = function () {
-    console.log("currentTimeDiff");
+var timeDiffObj = new timeDiffgetter();
+var currentTimeFill = function () {
+    timeDiffObj.currentTimeFillMethod();
 };
 var timeDiff = function () {
     console.log("timeDiff");
--- TimeCounter.ts
+++ TimeCounter.ts
@@ -1,9 +1,32 @@
 console.log("initialize");
 class timeDiffgetter {
-
+  startTime: string = '';
+  endTime: string = '';
+ public currentTimeFillMethod():void {
+    var today = new Date();
+    this.startTime = ((today.getHours() < 10) ? "0" : "") + today.getHours() + ":" + ((today.getMinutes() < 10) ? "0" : "") + today.getMinutes()+ ':' + ((today.getSeconds() < 10) ? "0" : "") + today.getSeconds();
+    this.setInput("startTime", this.startTime);
+  }
+  setInnerHTML(typeValue: string, value: string, type: string = "id")
+  {
+    switch(type) {
+      case 'id': 
+        document.getElementById(typeValue).innerHTML = value;
+        break;
+    }
+  }
+  setInput(typeValue: string, value: string, type: string = "id")
+  {
+    switch(type) {
+      case 'id': 
+        (<HTMLInputElement>document.getElementById(typeValue)).value = value;
+        break;
+    }
+  }
 }
-const currentTimeDiff = () => {
-  console.log("currentTimeDiff");
+var timeDiffObj = new timeDiffgetter();
+const currentTimeFill = () => {
+  timeDiffObj.currentTimeFillMethod();
 }
 
 const timeDiff = () => {
