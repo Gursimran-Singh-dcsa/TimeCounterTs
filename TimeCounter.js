007ed71db95d5cd5b20f9346486240f7038e2ac7
gursimran singh <gursimransingh8075@gmail.com>
2019-07-18 16:34:57 +0530

    add mock functionality

--- TimeCounter.js
+++ TimeCounter.js
@@ -1,1 +1,15 @@
 console.log("initialize");
+var timeDiffgetter = /** @class */ (function () {
+    function timeDiffgetter() {
+    }
+    return timeDiffgetter;
+}());
+var currentTimeDiff = function () {
+    console.log("currentTimeDiff");
+};
+var timeDiff = function () {
+    console.log("timeDiff");
+};
+var reset = function () {
+    console.log("reset");
+};
--- TimeCounter.ts
+++ TimeCounter.ts
@@ -1,1 +1,15 @@
 console.log("initialize");
+class timeDiffgetter {
+
+}
+const currentTimeDiff = () => {
+  console.log("currentTimeDiff");
+}
+
+const timeDiff = () => {
+  console.log("timeDiff");
+}
+
+const reset = () => {
+  console.log("reset");
+}
